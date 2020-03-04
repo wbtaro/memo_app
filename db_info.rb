bb4ea833f1a1bb6c4e577b82a3f58f341911b106
Matsushima Ryotaro <m.ryotaro.325@gmail.com>
2020-03-04 15:39:31 +0900

    Fix: edit files to store memos to PostgreSQL

---
+++ db_info.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+module DbInfo
+  HOST = "13.78.57.122"
+  DATABASE = "postgres"
+  USER = "xxxxx"
+  PASSWORD = "xxxxx"
+end
--- memo.rb
+++ memo.rb
@@ -1,58 +1,74 @@
 # frozen_string_literal: true
 
 require "securerandom"
+require "pg"
+require "./db_info"
 
 class Memo
-  MEMO_DIR = __dir__ + "/memo"
+  # 接続情報をハッシュに詰める
+  CONNECT_STRING = {
+    host: DbInfo::HOST,
+    user: DbInfo::USER,
+    dbname: DbInfo::DATABASE,
+    password: DbInfo::PASSWORD
+  }
   attr_reader :title, :id, :content
 
   # メモ一覧を返却
   def self.list
     memo_list = []
-    id_list.each do |id|
-      File.open(MEMO_DIR + "/" + id) do |file|
-        # メモの1行目がタイトル代わり
-        memo_list << Memo.new(file.read.split("\n")[0], id)
+    conn = PG.connect CONNECT_STRING
+    conn.exec("Select * from memo") do |result|
+      result.each do |row|
+        p row
+        memo_list << Memo.new(row["text"].split("\n")[0], row["id"], row["text"])
       end
     end
+    conn.close
     memo_list
   end
 
   # メモの内容を読み取る
   def self.read(id)
-    File.open(MEMO_DIR + "/" + id) do |file|
-      content = file.read
-      Memo.new(content.split("\n")[0], id, content)
+    memo = ""
+    conn = PG.connect CONNECT_STRING
+    conn.exec("Select * from memo where id='#{id}'") do |result|
+      result.each do |row|
+        memo = Memo.new(row["text"].split("\n")[0], row["id"], row["text"])
+      end
     end
+    conn.close
+    memo
   end
 
   # メモを新規作成
   def self.create(content)
-    File.open(MEMO_DIR + "/" + new_memo_id, "w") { |file| file.print content }
+    conn = PG.connect CONNECT_STRING
+    conn.exec("Insert into  memo values ('#{new_memo_id}', '#{content}')")
+    conn.close
   end
 
   # メモを編集
   def self.edit(id, content)
-    File.open(MEMO_DIR + "/" + id, "w") { |file| file.print content }
+    conn = PG.connect CONNECT_STRING
+    conn.exec("Update memo set text='#{content}' where id = '#{id}'")
+    conn.close
   end
 
   # メモを削除
   def self.delete(id)
-    File.delete(MEMO_DIR + "/" + id)
+    conn = PG.connect CONNECT_STRING
+    conn.exec("Delete from memo where id = '#{id}'")
+    conn.close
   end
 
-  def initialize(title, file_name, content = "")
+  def initialize(title, id, content = "")
     @title = title
-    @id = file_name
+    @id = id
     @content = content
   end
 
   private
-    # メモファイルの一覧を返す
-    def self.id_list
-      Dir.children(MEMO_DIR)
-    end
-
     # 新規作成するメモのIDを決定
     def self.new_memo_id
       SecureRandom.uuid
